30778b505d404d00b33bdcd0deb134011a7ceb16
Nuno Neves <youknownuno@gmail.com>
2018-03-22 18:43:43 -0700

    fixed a few redundancies, keeping the original ES5 naive solution

--- script/index.js
+++ script/index.js
@@ -1,20 +1,20 @@
 //===========================================//
 //============= VAR INITIATION ==============//
 //===========================================//
-
-var heroName = 'hero';
 var shopVisits = 0;
 var goldReward = 0;
-
+var units = {};
+var items = {};
+var heroName;
 //================== names =================//
 
-var nameArray = [
+var names = [
   'Noob-Noob', 'Sik', 'Krak', 'Steven', 'Pablo', 'Courtney', 'Weimin',
   'Charles', 'Rick', 'Morty', 'Mom', 'Dad', 'Lebowski', 'Mickey', 'Marco'];
 
 //=================== items =================//
 
-potion = {
+items.potion = {
   name: 'potion',
   power: 8,
   cost: 3,
@@ -25,27 +25,28 @@ potion = {
 //============= GAME FUNCTIONS ==============//
 //===========================================//
 
-function listFunctions() {
 
+//========= list of all functions ===========//
+
+function listFunctions() {
   console.log('help()');
   console.log('roll(numberOfDice,sides)');
   console.log('generateName()');
-  console.log('createUnit(unitNameString, /*strength*/ roll(numberOfDice,sides), armorValue, /*gold*/ roll(numberOfDice,sides))');
   console.log('checkUnits()');
-  console.log('deathMatch(unitA, unitB)');
+  console.log('battle(unitA, unitB)');
   console.log('shop()');
   console.log('buy(itemID)');
   console.log('revive(unit)');
+  console.log('createUnit(unitName, strength, armorValue, gold)');
 }
 
 //================== help ===================//
 
 function help() {
-
  console.log("Type your hero's name to see your hero's stats and loot");
  console.log("Type checkUnits() to see the list of units");
  console.log("Type createUnit() to make your own beast");
- console.log("Type deathMatch(attacker,defender) to witness a battle do the death");
+ console.log("Type battle(first,second) to witness a battle do the death");
  console.log("Type listFunctions() to see the complete list of functions");
 }
 
@@ -66,10 +67,14 @@ var roll = function(numberOfDice, sides) {
 
 function createHero() {
 
-  heroName = prompt("What is your name?", "hero");
+  while(heroName === undefined) {
+    heroName = prompt("What is your name?", "hero");
+  }
+  
+
   var heroStrength = roll(4,4);
 
-  window[heroName] = {
+  units[heroName] = {
     name: heroName,
     strength: heroStrength,
     maxStrength : heroStrength,
@@ -78,37 +83,34 @@ function createHero() {
     loot: {potion : { amount : 1 }}
   };
 
-  availableUnits.push(window[heroName]);
-
-  return window[heroName];
+  return units[heroName];
 }
 
 //===========================================//
 //=============== CREATE UNITS ==============//
 //===========================================//
 
-function createUnit(unitNameString, rollStrength, armorValue, rollGold, loot) {
+function createUnit(unitName, strengthRoll, armorValue, goldRoll, loot) {
 
-  window[unitNameString] = {
-    name: unitNameString,
-    strength: rollStrength,
-    maxStrength : rollStrength,
+  units[unitName] = {
+    name: unitName,
+    strength: strengthRoll,
+    maxStrength : strengthRoll,
     armor: armorValue,
-    gold: rollGold,
+    gold: goldRoll,
     loot: {}
   };
 
-  availableUnits.push(window[unitNameString]);
-  console.log("Congratulations! " + window[unitNameString].name+ " was born!");
+  console.log("Congratulations! " + units[unitName].name+ " was created!");
 
-  return window[unitNameString];
+  return units[unitName];
 }
 
 //============== generate name ==============//
 
 function generateName() {
 
-  var name = nameArray[Math.floor(Math.random() * nameArray.length)];
+  var name = names[Math.floor(Math.random() * names.length)];
 
   return name;
 }
@@ -117,100 +119,70 @@ function generateName() {
 
 function checkUnits() {
 
-  for (var i = 0; i < availableUnits.length; i++) {
-    console.log('');
-    console.log(availableUnits[i].name);
-    console.log(availableUnits[i]);
-  }
+  console.log(units);
   console.log('');
-  console.log('make them fight to death with the deathMatch(unit1,unit2) function!');
+  console.log('make them fight to death with the battle() function!');
+  console.log('example: battle("direwolf" , "orc")');
 }
 
 //===========================================//
 //================ COMBAT ===================//
 //===========================================//
 
-//======== check if units are valid =========//
-
-function validateTargets(attacker, defender) {
-
-  if (attacker.name === undefined) {
-    console.log("invalid attacker");
-    return false;
-
-  } else if (defender.name === undefined) {
-    console.log("invalid defender");
-    return false;
-
-  } else if ((attacker.strength <= 0) && (defender.strength <= 0)) {
-    console.log("Undead battles not implemented yet. Pick two living units instead");
-    return false;
-
-  } else if (defender.strength <= 0) {
-    console.log("defender is already dead!");
-    return false;
-
-  } else if (attacker.strength <= 0) {
-    console.log("attacker is already dead!");
-    return false;
-  }
-  return true;
-}
-
 //=============== hit check =================//
 
-function checkHit(attacker, defender) {
+function checkHit(first, second) {
 
   var hitRoll = roll(1, 6);
 
-  if (hitRoll > defender.armor) {
+  if (hitRoll > second.armor) {
     return true;
   }
-  console.log(attacker.name + ' failed to hit ' + defender.name + '.');
+  console.log(first.name + ' failed to hit ' + second.name + '.');
 
   return false;
 }
 
 //============ damage calculator ============//
 
-function checkDamage(attacker, defender) {
+function checkDamage(first, second) {
 
-  var damage = roll(1, attacker.strength); // reintroduce armor later
+  var damage = roll(1, first.strength); // reintroduce armor later
 
   if (damage <= 0) {
-    console.log(defender.name + " suffered no damage");
-    console.log(defender.name + "'s current strenght is " + defender.strength);
+    console.log(second.name + " suffered no damage");
+    console.log(second.name + "'s current strenght is " + second.strength);
     return false;
   }
 
-  defender.strength -= damage;
-  console.log(defender.name + ' suffered ' + damage + ' points of damage');
-  console.log(defender.name + "'s current strenght is " + defender.strength);
+  second.strength -= damage;
+  console.log(second.name + ' suffered ' + damage + ' points of damage');
+  console.log(second.name + "'s current strenght is " + second.strength);
   return true;
 }
 
-function checkForDeath(attacker, defender) {
+function checkForDeath(first, second) {
 
   goldReward = 0;
 
-  if ((defender.strength <= 0) && (attacker === window[heroName]) && (defender === window[heroName])) {
+  if ((second.strength <= 0) && (first === units[heroName]) && (second === units[heroName])) {
     console.log('You killed yourself. Why would you do that!?');
     return true;
 
-  } else if ((defender.strength <= 0) && (attacker.name === defender.name)) {
-    console.log(attacker.name + ' killed itself. What a dummy');
+  } else if ((second.strength <= 0) && (first.name === second.name)) {
+    console.log(first.name + ' killed itself. What a dummy');
     return true;
 
-  } else if (defender.strength <= 0) {
-    reward(defender, attacker);
-    console.log(defender.name + " died in combat");
-    console.log(attacker.name + " got " + goldReward + " gold");
+  } else if (second.strength <= 0) {
+    reward(second, first);
+    console.log(second.name + " died in combat");
+    console.log(first.name + " got " + goldReward + " gold");
     return true;
 
-  } else if (attacker.strength <= 0) {
-    reward(attacker, attacker);
-    console.log(attacker.name + " died in combat");
-    console.log(defender.name + " got " + goldReward + " gold");
+  } else if (first.strength <= 0) {
+    reward(first, first);
+    console.log(first.name + " died in combat");
+    console.log(second.name + " got " + goldReward + " gold");
     return true;
   }
 
@@ -219,29 +191,48 @@ function checkForDeath(attacker, defender) {
 
 //============== FIGHT ROUND ==============//
 
-function fight(attacker, defender) {
+function fight(first, second) {
 
-  if (!validateTargets(attacker, defender)) { return false; }
+  if (!checkHit(first, second)) { return true; }
 
-  if (!checkHit(attacker, defender)) { return true; }
+  if (!checkDamage(first, second)) { return true; }
 
-  if (!checkDamage(attacker, defender)) { return true; }
-
-  if (!checkForDeath(attacker, defender)) { return true; }
+  if (!checkForDeath(first, second)) { return true; }
 
   console.log("");
 
   return false;
 }
 
-//=============== DEATHMATCH ================//
+//=============== BATTLE ================//
 
-function deathMatch(attacker, defender) {
+function battle(first, second) {
 
-  while ((attacker.strength > 0) && (defender.strength > 0)) {
+  if(!units[first]) {
+      return "one of the units you've picked is already dead!";
+    }
 
-    if (!fight(attacker, defender)) { return; }
-    if (!fight(defender, attacker)) { return; }
+  if(!units[second]) {
+      return "one of the units you've picked is already dead!";
+    }
+
+  if(units[second].strength < 1 && units[first].strength < 1) {
+      return "undead battles are not yet a thing. Try living units, or run revive(unitName) and try again.";
+    }
+
+
+  if(units[first].strength < 1) {
+      return "the first unit you chose is already dead!";
+    }
+
+  if(units[second].strength < 1) {
+      return "the second unit you chose is already dead!";
+    }
+
+  while ((units[first].strength > 0) && (units[second].strength > 0)) {
+
+    if (!fight(units[first], units[second])) { return; }
+    if (!fight(units[second], units[first])) { return; }
   }
 
   return;
@@ -272,7 +263,7 @@ function revive(unit) {
 function shop() {
 
   if (shopVisits === 0) {
-    console.log("Shopkeeper: Welcome to the shop, " + window[heroName].name + "!");
+    console.log("Shopkeeper: Welcome to the shop, " + units[heroName].name + "!");
   }
   console.log("Shopkeeper: Type shopLoot to see a list of items.");
   console.log("Shopkeeper: Type buy(itemID) to purchase that item.");
@@ -288,7 +279,7 @@ function buy(itemID) {
     return "Shopkeeper: I'm afraid I don't have a " + itemID.name;
   }
 
-  if (window[heroName].gold <= itemID.cost) {
+  if (units[heroName].gold <= itemID.cost) {
     return "Shopkeeper: I'm afraid you don't have enough money for that.";
   }
 
@@ -297,9 +288,9 @@ function buy(itemID) {
   // test the above
 
   if (reply === "yes") {
-    window[heroName].gold -= itemID.cost;
-    window[heroName]['loot'][itemID.name]['amount']++; //only works for items that already exist (potions)
-    return itemID.name + " added to your loot. You have " + window[heroName].gold + " gold left.";
+    units[heroName].gold -= itemID.cost;
+    units[heroName]['loot'][itemID.name]['amount']++; //only works for items that already exist (potions)
+    return itemID.name + " added to your loot. You have " + units[heroName].gold + " gold left.";
   }
 
   return "Shopkeeper: Maybe next time, then.";
@@ -308,16 +299,16 @@ function buy(itemID) {
 //=================== use ==================//
 
 function use(itemID) {
-  if( (window[heroName].loot[itemID.name] === null) || (window[heroName].loot[itemID.name].amount === 0) ){
+  if( (units[heroName].loot[itemID.name] === null) || (units[heroName].loot[itemID.name].amount === 0) ){
     return "You don't have any " + itemID.name + "s with you.";
   }
 
     //for now let's go with potion
 
-window[heroName].strength += itemID.power;
-window[heroName].loot[itemID.name].amount --;
+units[heroName].strength += itemID.power;
+units[heroName].loot[itemID.name].amount --;
 console.log("You drank a " + itemID.name + " and recovered " + itemID.power + " strength points.");
-console.log(window[heroName].name + "'s current strength is " + window[heroName].strength);
+console.log(units[heroName].name + "'s current strength is " + units[heroName].strength);
 }
 
 //check kind of item and use accordingly. Maybe have a
@@ -329,8 +320,8 @@ console.log(window[heroName].name + "'s current strength is " + window[heroName]
 
 //=================== UNITS =================//
 
-var giant = {
-  name: 'giant ' + generateName(),
+units.giant = {
+  name: 'giant',
   strength: 16,
   maxStrength: 16,
   armor: 2,
@@ -338,8 +329,8 @@ var giant = {
   loot: {}
 };
 
-var orc = {
-  name: 'orc ' + generateName(),
+units.orc = {
+  name: 'orc',
   strength: 6,
   maxStrength: 6,
   armor: 3,
@@ -347,8 +338,8 @@ var orc = {
   loot: {}
 };
 
-var hobgoblin = {
-  name: 'hobgoblin ' + generateName(),
+units.hobgoblin = {
+  name: 'hobgoblin',
   strength: 9,
   maxStrength: 9,
   armor: 6,
@@ -356,8 +347,8 @@ var hobgoblin = {
   loot: {}
 };
 
-var direwolf = {
-  name: 'direwolf ' + generateName(),
+units.direwolf = {
+  name: 'direwolf',
   strength: 7,
   maxStrength: 7,
   armor: 5,
@@ -365,8 +356,8 @@ var direwolf = {
   loot: {}
 };
 
-var goblin = {
-  name: 'goblin ' + generateName(),
+units.goblin = {
+  name: 'goblin',
   strength: 3,
   maxStrength: 3,
   attack: 6,
@@ -399,13 +390,11 @@ if (typeof console._commandLineAPI !== 'undefined') {
 
 console.API.clear();
 
-
-var availableUnits = [goblin, orc, giant, direwolf, hobgoblin]; //move this somewhere else
 createHero();
 
-alert("Hello " + window[heroName].name + "! This is a hackable hack and slash!");
+alert("Hello " + units[heroName].name + "! This is a hackable hack and slash!");
 alert("Type help() to see the list of commands");
-console.log("Hello " + window[heroName].name + "! This is a hackable hack and slash!");
+console.log("Hello " + units[heroName].name + "! This is a hackable hack and slash!");
 console.log("Type help() to see the list of commands");
 
 //===========================================//
@@ -413,10 +402,9 @@ console.log("Type help() to see the list of commands");
 //===========================================//
 
 // make a critical hit (ignores armor)
-// fix endless loop if character deathmatches itself with less strength than armor
+// fix endless loop if character battlees itself with less strength than armor
 // test everything
 // make final boss
 // a unit reroll function
 // expand shop and item usage HoF
 // expand battle reward system
-// fix ''s and ""s
